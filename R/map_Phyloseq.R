eb4f56bd871368a939e88eba42c2204b2b79f7f1
zach powers <zcharlop@rockefeller.edu>
2014-06-04 16:38:26 -0400

    map_network reworking. updated maP-physeq

--- R/map_Phyloseq.R
+++ R/map_Phyloseq.R
@@ -4,23 +4,28 @@
 #' nodes is below a potentially arbitrary threshold,
 #' and special care should be given to considering the choice of this threshold.
 #'
-#' @usage map_phylo(physeq, type="samples", 
-#'   color=NULL, shape=NULL, point_size=4, alpha=1,
+#' @usage map_phylseq(physeq, type="samples",  region=NULL,
+#'   color=NULL, shape=NULL, pointsize=4, pointalpha=1,
 #'   label="value", hjust = 1.35, 
 #'   line_weight=0.5, line_color=color, line_alpha=0.4,
 #' 	layout.method=layout.fruchterman.reingold, title=NULL)
 map_phyloseq <- function(physeq, region=NULL, color=NULL, pointalpha = 0.8, pointsize=NULL){
   #check basic physeq and lat/lon
-  .check_physeq(physeq)
+  latlon <- .check_physeq(physeq)
+  latcol <- latlon[1]
+  loncol <- latlon[2]
   
   sampledata <- sample_data(physeq)
   names <- names(sampledata)
   
   #check that color and pointsize are present and that pointsize is numeric 
-  if (!color %in% names) { stop("color variable must be a sampledata column") }
-  if (!pointsize %in% names) { stop("color variable must be a sampledata column") }
+  if (!is.null(color){
+    if(!color %in% names) { stop("color variable must be a sampledata column") }
+  }
+  if (!is.null(color){
+    if (!pointsize %in% names) { stop("color variable must be a sampledata column") }
+  }
   if (sample_data[pointsize][1] %in% names) { stop("color variable must be a sampledata column") }
-  
   
   #basemap
   if (!is.null(region)){
@@ -36,7 +41,6 @@ map_phyloseq <- function(physeq, region=NULL, color=NULL, pointalpha = 0.8, poin
     data2 <- data2[ data2$Longitude > minlong, ]
     data2 <- data2[ data2$Latitude < maxlat, ]
     data2 <- data2[ data2$Latitude > minlat, ]
-    
   } else {
     world <- map_data("world")
   }
@@ -52,6 +56,7 @@ map_phyloseq <- function(physeq, region=NULL, color=NULL, pointalpha = 0.8, poin
            axis.title=element_blank())
   
   data = sample_data(physeq)
+  #how to hande when pointsize information can be either global (outside of aes), orper-sample (inseide of aes)
   if (!is.null(pointsize) ) {
     worldmap <- worldmap + geom_point(data=data, aes_string( x=loncol, y=latcol, group=names(data)[1], color=color, size = pointsize), alpha= pointalpha)    
   } else {
@@ -65,21 +70,16 @@ map_phyloseq <- function(physeq, region=NULL, color=NULL, pointalpha = 0.8, poin
 #' nodes is below a potentially arbitrary threshold,
 #' and special care should be given to considering the choice of this threshold.
 #'
-#' @usage draw_network(physeq, type="samples", maxdist=0.9, distance="jaccard"
+#' @usage dmap_network(physeq,maxdist=0.9, distance="jaccard"
 #'   color=NULL, shape=NULL, region=NULL, point_size=4, alpha=1,
 #'   label="value", hjust = 1.35, 
 #' 	line_weight=0.5, line_color=color, line_alpha=0.4,
 #' 	layout.method=layout.fruchterman.reingold, title=NULL)
-map_network <- function(physeq, maxdist=0.9, distance="jaccard" ,sampledata, color=NULL, region=NULL, pointsize=4, pointalpha = 0.8){
-  .check_physeq(physeq)
-  
-  #get data and plot
-  samples <- as.character(network$data$value) 
-  data2 <- sampledata[ row.names(sampledata) %in% samples ]
-  
-  #create network
-  ig <- make_network(physeq, max.dist = maxdist, distance=distance)
-  networkplot <- plot_network(ig, ad35, color=j)
+map_network <- function(physeq, maxdist=0.9, distance="jaccard", color=NULL, region=NULL, pointsize=4, pointalpha = 0.8){
+  latlon <- .check_physeq(physeq)
+  latcol <- latlon[1]
+  loncol <- latlon[2]
+  sampledata <- sample_data(physeq)
   
   #helper function to assign names in the df to clusters
   get_clusters <- function(num, graph=ig){
@@ -89,42 +89,39 @@ map_network <- function(physeq, maxdist=0.9, distance="jaccard" ,sampledata, col
     names   <- get.vertex.attribute(graph, 'name', members)
     df = data.frame(names)
     df['cluster'] <- num
+    rownames(df) <- df$names
+   #return a df with name/cluster columns
     df
   }
   
   #create df from cluster membership
+  ig <- make_network(physeq, max.dist = maxdist, distance=distance)
   clusts <- seq(clusters(ig)$no)
   clustdf <- Reduce( rbind, Map(get_clusters, clusts))
-  
-  
+    
   #merge the original dataframe with the cluster info
-  data <- merge(sampledata, )
+  data <- merge(clustdf, data.frame(sampledata), by="row.names", all.x=T)
   
+#   #basemap
+#   if (!is.null(region)){
+#     world <- map_data("world", region = region)
+#     
+#     #ToDO: allow subsetting of sampels by region. Is there a point-in-polygon library?
+#     #this is a quick filter based on latitude and longitude not point-in-polygon
+#     maxlat  = max(world$lat)
+#     minlat  = min(world$lat)
+#     maxlong = max(world$long)
+#     minlong = min(world$long)
+#     data2 <- data2[ data2$Longitude < maxlong, ]
+#     data2 <- data2[ data2$Longitude > minlong, ]
+#     data2 <- data2[ data2$Latitude < maxlat, ]
+#     data2 <- data2[ data2$Latitude > minlat, ]
+#     
+#   } else {
+#     world <- map_data("world")
+#   }
   
-      ggsave(network_AD, filename=paste('../../presentations/Rpresentations/Pictures/ad35_network_',j,'_',as.character(i),'.png',sep=""),
-             width=8,height=6)
-    }}
-  
- 
-  #basemap
-  if (!is.null(region)){
-    world <- map_data("world", region = region)
-    
-    #ToDO: allow subsetting of sampels by region. Is there a point-in-polygon library?
-    #this is a quick filter based on latitude and longitude not point-in-polygon
-    maxlat  = max(world$lat)
-    minlat  = min(world$lat)
-    maxlong = max(world$long)
-    minlong = min(world$long)
-    data2 <- data2[ data2$Longitude < maxlong, ]
-    data2 <- data2[ data2$Longitude > minlong, ]
-    data2 <- data2[ data2$Latitude < maxlat, ]
-    data2 <- data2[ data2$Latitude > minlat, ]
-    
-  } else {
-    world <- map_data("world")
-  }
-  
+  world <- map_data("world")
   worldmap <- ggplot(world, aes(x=long, y=lat, group=group)) +
     geom_polygon( fill="grey",alpha=0.6) +
     scale_y_continuous(breaks=(-2:2) * 30) +
@@ -134,8 +131,8 @@ map_network <- function(physeq, maxdist=0.9, distance="jaccard" ,sampledata, col
            axis.ticks = element_blank(), 
            axis.line = element_blank(), 
            axis.title=element_blank())
-  
-  worldmap + geom_point(data=data2, aes( x=Longitude, y=Latitude, color=Geotype, group=Geotype), size=pointsize, alpha= pointalpha)
+worldmap <- worldmap + geom_point(data=data, aes_string( x=loncol, y=latcol, group=names(data)[1], color=color), size=4 ,alpha=pointalpha)    
+  worldmap + geom_point(data=data, aes( x=Longitude, y=Latitude, color=cluster, group=Geotype), size=pointsize, alpha= pointalpha)
   
 }
 #' Plot a network using ggplot2 (represent microbiome)
@@ -193,7 +190,7 @@ points_from_network <- function(network, sampledata) {
 .check_physeq <- function(physeq){
   #check phyloseq objects for Lat/Lon
   if (!"sam_data" %in% getslots.phyloseq(physeq)){
-    stop("Mapping requires that phyloseq objects have Sample_Data with Latitude and Longitude ")
+    stop("Mapping requires that phylos objects have Sample_Data with Latitude and Longitude ")
   } 
   
   #check that sampledata has latitude and longitude columns
@@ -217,5 +214,5 @@ points_from_network <- function(network, sampledata) {
   }
   if (lat_present == FALSE) { stop("sampledata must have a valid latitude column") }
   if (lon_present == FALSE) { stop("sampledata must have a valid longitude column")  }
-  
+  list(latcol, loncol)
 }
