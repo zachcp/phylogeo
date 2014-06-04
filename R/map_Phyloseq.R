938b4794822e1dc18de4a30ac219c14df4970a89
zach powers <zcharlop@rockefeller.edu>
2014-06-04 15:50:12 -0400

    refaactor basic physeq checking

--- R/map_Phyloseq.R
+++ R/map_Phyloseq.R
@@ -9,32 +9,18 @@
 #'   label="value", hjust = 1.35, 
 #'   line_weight=0.5, line_color=color, line_alpha=0.4,
 #' 	layout.method=layout.fruchterman.reingold, title=NULL)
-map_phyloseq <- function(physeq, region=NULL, color=NULL, pointalpha = 0.8, pointsize=4){
-  #check that phylobject has sample data
-  if (!"sam_data" %in% getslots.phyloseq(physeq)){
-    stop("Mapping requires that phyloseq objects have Sample_Data with Latitude and Longitude ")
-  } 
-  sampledata <- sample_data(physeq)
+map_phyloseq <- function(physeq, region=NULL, color=NULL, pointalpha = 0.8, pointsize=NULL){
+  #check basic physeq and lat/lon
+  .check_physeq(physeq)
   
-  #check that sampledata has latitude and longitude columns
-  lat <- c('latitude', 'lat', 'lattitude')
-  lon <- c('longitude', 'lon', 'long')
-  lat_present = FALSE
-  lon_present = FALSE
+  sampledata <- sample_data(physeq)
   names <- names(sampledata)
   
-  for (name in names){
-    if( tolower(name) %in% lat){
-      latcol <- name
-      lat_present <- TRUE
-    }
-    if( tolower(name) %in% lon){
-      loncol <- name
-      lon_present <- TRUE
-    }
-  }
-  if (lat_present == FALSE) { stop("sampledata must have a valid latitude column") }
-  if (lon_present == FALSE) { stop("sampledata must have a valid longitude column")  }
+  #check that color and pointsize are present and that pointsize is numeric 
+  if (!color %in% names) { stop("color variable must be a sampledata column") }
+  if (!pointsize %in% names) { stop("color variable must be a sampledata column") }
+  if (sample_data[pointsize][1] %in% names) { stop("color variable must be a sampledata column") }
+  
   
   #basemap
   if (!is.null(region)){
@@ -66,25 +52,60 @@ map_phyloseq <- function(physeq, region=NULL, color=NULL, pointalpha = 0.8, poin
            axis.title=element_blank())
   
   data = sample_data(physeq)
-  worldmap + geom_point(data=data, aes_string( x=loncol, y=latcol, group=names(data)[1], color=color), size=pointsize, alpha= pointalpha)
-  
+  if (!is.null(pointsize) ) {
+    worldmap <- worldmap + geom_point(data=data, aes_string( x=loncol, y=latcol, group=names(data)[1], color=color, size = pointsize), alpha= pointalpha)    
+  } else {
+    worldmap <- worldmap + geom_point(data=data, aes_string( x=loncol, y=latcol, group=names(data)[1], color=color), size=4 ,alpha=pointalpha)    
+  }
+  worldmap
 }
-#' Draw A Map from a Phyloseq Object
+#' Create a Network from the Phyloseq Objects and Draw A Map of the Clusters
 #'
 #' In this case, edges in the network are created if the distance between
 #' nodes is below a potentially arbitrary threshold,
 #' and special care should be given to considering the choice of this threshold.
 #'
-#' @usage draw_map(physeq, type="samples", 
-#'   color=NULL, shape=NULL, point_size=4, alpha=1,
+#' @usage draw_network(physeq, type="samples", maxdist=0.9, distance="jaccard"
+#'   color=NULL, shape=NULL, region=NULL, point_size=4, alpha=1,
 #'   label="value", hjust = 1.35, 
 #' 	line_weight=0.5, line_color=color, line_alpha=0.4,
 #' 	layout.method=layout.fruchterman.reingold, title=NULL)
-draw_map <- function(network, sampledata, region=NULL, pointsize=4, pointalpha = 0.8){
+map_network <- function(physeq, maxdist=0.9, distance="jaccard" ,sampledata, color=NULL, region=NULL, pointsize=4, pointalpha = 0.8){
+  .check_physeq(physeq)
+  
   #get data and plot
   samples <- as.character(network$data$value) 
   data2 <- sampledata[ row.names(sampledata) %in% samples ]
   
+  #create network
+  ig <- make_network(physeq, max.dist = maxdist, distance=distance)
+  networkplot <- plot_network(ig, ad35, color=j)
+  
+  #helper function to assign names in the df to clusters
+  get_clusters <- function(num, graph=ig){
+    #num is the index of the cluster
+    clusts  <- clusters(graph)
+    members <- which(clusts$membership == num) #get membership
+    names   <- get.vertex.attribute(graph, 'name', members)
+    df = data.frame(names)
+    df['cluster'] <- num
+    df
+  }
+  
+  #create df from cluster membership
+  clusts <- seq(clusters(ig)$no)
+  clustdf <- Reduce( rbind, Map(get_clusters, clusts))
+  
+  
+  #merge the original dataframe with the cluster info
+  data <- merge(sampledata, )
+  
+  
+      ggsave(network_AD, filename=paste('../../presentations/Rpresentations/Pictures/ad35_network_',j,'_',as.character(i),'.png',sep=""),
+             width=8,height=6)
+    }}
+  
+ 
   #basemap
   if (!is.null(region)){
     world <- map_data("world", region = region)
@@ -167,4 +188,34 @@ points_from_network <- function(network, sampledata) {
   samples <- as.character(network$data$value) 
   data <- sampledata[ row.names(sampledata) %in% samples ]
   points <- points(data$Longitude, data$Latitude, pch=20, col="gray50", cex=1) 
+}
+#
+.check_physeq <- function(physeq){
+  #check phyloseq objects for Lat/Lon
+  if (!"sam_data" %in% getslots.phyloseq(physeq)){
+    stop("Mapping requires that phyloseq objects have Sample_Data with Latitude and Longitude ")
+  } 
+  
+  #check that sampledata has latitude and longitude columns
+  lat <- c('latitude', 'lat', 'lattitude')
+  lon <- c('longitude', 'lon', 'long')
+  lat_present = FALSE
+  lon_present = FALSE
+  
+  sampledata <- sample_data(physeq)
+  names <- names(sampledata)
+  
+  for (name in names){
+    if( tolower(name) %in% lat){
+      latcol <- name
+      lat_present <- TRUE
+    }
+    if( tolower(name) %in% lon){
+      loncol <- name
+      lon_present <- TRUE
+    }
+  }
+  if (lat_present == FALSE) { stop("sampledata must have a valid latitude column") }
+  if (lon_present == FALSE) { stop("sampledata must have a valid longitude column")  }
+  
 }
