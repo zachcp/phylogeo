a6c07649aa03980658666445059598986c01f87e
zach powers <zcharlop@rockefeller.edu>
2014-08-09 19:56:50 -0400

    phyloseq geo file for keeping track if datasets and for package-level documentation

--- R/phyloseq_geo.R
+++ R/phyloseq_geo.R
@@ -4,3 +4,13 @@
 #' @import phyloseq
 #' @keywords package
 
+#' Phyloseq Object for Bat Guano
+#'
+#' A phyloseq object with data collected from the bat guano experiments
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name diamonds
+#' @usage data()
+#' @format A data frame with 53940 rows and 10 variables
+NULL
