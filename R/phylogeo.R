a7e32416028cc5a7e472ebd1eb06f22160490e00
zach powers <zcharlop@rockefeller.edu>
2014-08-11 13:24:25 -0400

    rename phylogeo

--- R/phylogeo.R
+++ R/phylogeo.R
@@ -1,12 +1,11 @@
 ###############################################
 #'  Mapping of microbiome data.
 #'
-#' @name phyloseq.geo
+#' @name phylogeo
 #' @author Zach Charlop-Powers \email{zcharlop@@rockefeller.edu}
 #' @docType package
 #' @import phyloseq
 #' @keywords package
-
 
 #' Phyloseq Object for Bat Guano
 #'
