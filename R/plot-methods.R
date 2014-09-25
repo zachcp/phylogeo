1836ab107bc448f27c08e239fae949d51c5f25b0
zach powers <zcharlop@rockefeller.edu>
2014-09-25 15:05:44 -0400

    typos

--- R/plot-methods.R
+++ R/plot-methods.R
@@ -41,11 +41,11 @@ plot_greatcircle_distance <- function(physeq, distancemethod="jaccard"){
     geodistances <- sp::spDists(df2, longlat=TRUE)
     colnames(geodistances) <- row.names(df2)
     row.names(geodistances) <- row.names(df2)
-    geodistances <- phlyogeo:::.dist_to_edge_table(geodistances, dname = "geodist")
+    geodistances <- phylogeo:::.dist_to_edge_table(geodistances, dname = "geodist")
     
     #get ecologicaldistances
     ecodistance <- distance(physeq, method = distancemethod)
-    ecodistance <- phlyogeo:::.dist_to_edge_table(ecodistance, dname="ecodist" )
+    ecodistance <- phylogeo:::.dist_to_edge_table(ecodistance, dname="ecodist" )
     
     #make mergeable names for the two distance functions and merge
     concatvals <- function(x,y){ return(paste(x,"_",y,sep=""))}
