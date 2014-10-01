8f95e9ea006f83fab33b6e2f1aba198711c49bc6
zach powers <zcharlop@rockefeller.edu>
2014-10-01 17:44:04 -0400

    begin a set of tests for plot objects

--- tests/testthat/test_map_plotting_functions.R
+++ tests/testthat/test_map_plotting_functions.R
@@ -1,9 +1,39 @@
 ################################################################################
 # Use testthat to test phyloseq data objects plotting functions
 ################################################################################
-library("phylogeo"); library("testthat")
-# # # # TESTS!
+library("phylogeo"); library("testthat"); library(ggplot2); library(igraph)
+# # # # TESTS!;
 
+test_that("map_phyloseq: results in a ggplot", {
+  data(batfecal)
+	p1 <- map_phyloseq(batfecal)
+	# ggplot-class tests
+	expect_is(p1, "ggplot")
+	expect_is(print(p1), "list")
+	})
+
+test_that("map_tree: tree_objects are present when mapping a phylogenetic tree", {
+  data(batfecal)
+  data(epoxamicin_KS)
+	p1 <- map_tree(epoxamicin_KS)
+	# ggplot-class tests
+  expect_error(map_tree(batfecal))
+	expect_is(p1, "ggplot")
+	expect_is(print(p1), "list")
+	})
+    
+test_that("map_network: can use a precalculated network", {
+  data(batmicrobiome)
+  p1 <- map_network(batmicrobiome)
+  ig <- make_network(batmicrobiome)
+  p2 <- map_network(batmicrobiome, igraph=ig)
+	# ggplot-class tests
+  expect_is(p1, "ggplot")
+  expect_is(p2, "ggplot")
+  expect_is(ig, "igraph")
+  expect_is(print(p1), "list")
+  expect_is(print(p2), "list")
+	})
 
 # Test the following functions
 # map_phyloseq <- function(physeq, region=NULL, color=NULL, shape=NULL, point_size=4, alpha = 0.8, jitter=FALSE, jitter.x=3, jitter.y=3){
