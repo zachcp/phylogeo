c6b1aaa481e50a7d8e74eebb10b20f70461e0c3d
zach powers <zcharlop@rockefeller.edu>
2015-02-06 16:52:25 -0500

    update some tests

--- tests/testthat/test_utility_functions.R
+++ tests/testthat/test_utility_functions.R
@@ -10,8 +10,6 @@ context("Utility Functions")
 # Test the following funcitons
 # .check_physeq <- function(physeq){
 # .create_basemap <-function(region, df, latcol, loncol){
-# .check_names <- function(member, df, allownumeric=FALSE){
-# .jitter_df <- function(df, xcol, ycol, jitter.x, jitter.y){
 # .check_NA <- function(df, col){
 # .coerce_numeric <- function(df, col)
 # .dist_to_edge_table = function(Dist, dname = "dist")
@@ -39,4 +37,28 @@ test_that("phyloseq objects have latitude and longitude columns", {
   bat_lon <- batfecal
   sample_data(bat_lon) <- dflon
   expect_error(.check_physeq(bat_lon))
+})
+
+test_that("jittering works and uses the seed correctly", {
+  df <- data.frame(x= c(1,2,3,4,5), y=c(3,4,5,6,7))
+  df2 <- data.frame(x= c(1,2,3,4,5), y=c(3,4,5,6,7))
+  df_j <- .jitter_df(df, "x","y",3,4,123)
+  df2_j <- .jitter_df(df, "x","y",3,4,123)
+  
+  expect_is(df_j , "data.frame")
+  expect_is(df2_j , "data.frame")
+  expect_equal(df_j, df2_j)
+})
+
+test_that("checknames trips an error when its supposed to",{
+  df <- data.frame(col1=c(1,2,3),
+                   col2=c(1,2,3),
+                   4=c(1,2,3))
+  
+  expect_true(.check_names("col1"))
+  expect_true(.check_names("col2"))
+  expect_true(.check_names(4, allownumeric = TRUE))
+  expect_warning(.check_names("col3"), "col3 variable must be a valid column name of a Phyloseq table")
+  expect_warning(.check_names(1), "1 variable must be a valid column name of a Phyloseq table")
+  expect_warning(.check_names(4), "1 variable must be a valid column name of a Phyloseq table")
 })
