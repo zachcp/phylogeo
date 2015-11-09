db306a1efd5e6a3c63a2f9dc225ef8571586c191
zach powers <zcharlop@rockefeller.edu>
2015-11-09 16:37:09 -0500

    new roxygen doc comments
    get d3 example working from serialized phyloseq object data

---
+++ R/d3map_phyloseq.R
@@ -0,0 +1,129 @@
+#' create d3 Map
+#'
+#' @import phyloseq
+#' @importFrom jsonlite toJSON
+#' @export
+d3map_phyloseq <- function(physeq, tax_column, mapdata="USA", omitNA=TRUE){
+    if (!tax_column %in% colnames(tax_table(physeq))) {
+        stop("you must split on a column inthe taxtable")
+    }
+    possible_maps = c("USA","World50m", "World110m")
+    if (!mapdata %in% possible_maps) {
+        stop(paste0("mapdata must be one of ", possible_maps))
+    }
+
+    #create phygeo object
+    phygeo <- phylogeo(physeq)
+
+    # Tabulate the counts for each value in the tax_table column for each sample
+    genfac = factor(tax_table(phygeo)[, tax_column])
+    gentab = apply(otu_table(phygeo), MARGIN = 2, function(x) {
+        tapply(x, INDEX = genfac, FUN = sum, na.rm = TRUE, simplify = TRUE)
+    })
+
+    #create a dataframe where count data is a list of dataframe elements
+    # needed for JSON serializatio
+    # see https://cran.r-project.org/web/packages/jsonlite/vignettes/json-mapping.pdf
+    gendf <- data.frame(gentab)
+    samdata  <- data.frame(sample_data(phygeo))
+    # add explicit Latitude and Longitude (needed/used by D3.js)
+    colnames(samdata)[colnames(samdata)==phygeo@latitude] <- "Latitude"
+    colnames(samdata)[colnames(samdata)==phygeo@longitude] <- "Longitude"
+
+    if (omitNA) {samdata <- na.omit(samdata)}
+
+    #samplename and countdata will be used in d3.js. they should not be changed.
+    samdata['samplename'] <- rownames(samdata)
+    samdata['countdata'] <- lapply(samdata['samplename'],
+                                   function(x){ data.frame(t(gendf[x]))})
+
+    #mapdata
+    usa_10m = system.file("static/maps/us-10m.js", package="phylogeo");
+    world_50m = system.file("static/maps/world-50m.js", package="phylogeo");
+    world_100m = system.file("static/maps/world-110m.js", package="phylogeo");
+
+    #background data
+    deps <- system.file("static/js_css_dependencies.html", package="phylogeo");
+    basecss <- system.file("static/style.css", package="phylogeo");
+
+    # specific data
+    usa_basicmap_js = system.file("static/usamap.js", package="phylogeo");
+    world_basicmap_js = system.file("static/worldmap.js", package="phylogeo");
+
+
+    #choose the map
+    #"USA","World50m", "World110m"
+    if (mapdata == "USA") {
+        mapfile <- usa_10m
+        mapJS <- usa_basicmap_js
+    } else if(mapdata == "World50m") {
+        mapfile <- world_50m
+        mapJS <- world_basicmap_js
+    } else if (mapdata == "World100m") {
+        mapfile <- world_100m
+        mapJS <- world_basicmap_js
+    }
+
+    servedata(jsondata = toJSON(samdata),
+              mapfile = mapfile,
+              mapjsfile = mapJS,
+              basecss = basecss,
+              htmlfile = deps)
+}
+
+#' Wrapper to setup a D3 Map
+#'
+#' Starts the httpuv web server and hosts a simple form including a file
+#' upload to demo the multipart parser.
+#' Adapted from https://github.com/jeroenooms/webutils/blob/master/R/demo_httpuv.R
+#
+#' @export
+#' @importFrom stats runif
+#' @importFrom utils browseURL getFromNamespace head str tail
+#' @importFrom htmltools includeScript includeHTML includeCSS
+#' @param jsondata
+#' @param mapfile
+#' @param htmlfile
+#' @param mapjsfile
+#' @param basecss
+#' @param basecss
+servedata <- function(mapfile,
+                      htmlfile,
+                      mapjsfile,
+                      jsondata,
+                      basecss,
+                      port=8000){
+    rook_handler <- function(env){
+        # See Rook spec
+        content_type <- env[["CONTENT_TYPE"]]
+        http_method <- env[["REQUEST_METHOD"]]
+        request_body <- env[["rook.input"]]$read()
+        path <- env[["PATH_INFO"]]
+
+        # Show HTML page for GET requests.
+        if(http_method == "GET"){
+            message("Received HTTP GET request: ", path)
+            list (
+                status = 200,
+                body = paste(includeHTML(htmlfile),
+                             includeScript(mapfile),
+                             paste0("<script>var pointdata = ", jsondata ,"</script>"),
+                             includeScript(mapjsfile),
+                             includeCSS(basecss),
+                             paste0("<body onload=drawMap()>"),
+                             paste0("<select></select>"),
+                             paste0("</body>"),
+                             collapse="\n"),
+                headers = c("content-type" = "text/html")
+            )
+        }
+    }
+
+    # Start httpuv
+    if(missing(port))
+        port <- round(runif(1, 2e4, 5e4));
+    url <- paste0("http://localhost:", port, "/")
+    message("Opening ", url)
+    browseURL(url)
+    httpuv::runServer("0.0.0.0", port, list(call = rook_handler))
+}
